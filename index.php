6f9200f59c8babc3e01f41abc8b226346258a0bd
123456430 <153580722+123456430@users.noreply.github.com>
2023-12-15 11:53:10 +1100

    глянь правильное ли начало

--- index.php
+++ index.php
@@ -1,19 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
 <body>
-    
-</body>
-
-<?php 
-$str= "имя";
-echo "$str";
-echo "<input type= 'text'>" . '<br>';
-$st ="фамилия";
-echo "$st";
-echo "<input type = 'text'>". '<br>';
+  <?php
+echo "ФИО";
+echo "<input tupe = 'text'>" . '<br>';
+echo "дата рождения";
+echo "<input tupe = 'text'>" . '<br>';
+   ?>
+   <?php
+   echo "<button> нажми меня</button>";
+   ?>
+   <form method="post">
+    <button type="submit" name="button>нажми меня</button>;
+    <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL); 
+    error_reporting(E_ALL); 
+    ?>
+  <?php
+  try {
+      // подключаемся к серверу
+      $conn = new PDO ("mysql:host = 127.0.0.1;port=3306; mypassword = ");  
+      echo "Database connection established";
+    }
+    catch (PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
 ?>
+$sth = $dbh->prepare("SELECT * FROM `category` WHERE `id` = :id");
+$sth->execute(array('id' => '21'));
+$array = $sth->fetch(PDO::FETCH_ASSOC);
+print_r($array);
